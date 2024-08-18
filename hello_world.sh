bb7a626686687eef201be6a30a34c9fb5903e2ca
Jay Bazuzi <jay@bazuzi.com>
2024-08-17 22:52:55 -0700

    @ F dotnet hello world

---
+++ hello_world.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+set -euo pipefail
+
+curl --fail --silent https://dot.net/v1/dotnet-install.sh | bash -s -- --jsonfile global.json
+"$HOME/.dotnet/dotnet" tool install dotnet-script > /dev/null
+
+"$HOME/.dotnet/dotnet" script hello_world.csx
