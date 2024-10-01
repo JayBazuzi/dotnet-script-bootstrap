78e7d97f174ce0ce064160278569eceb5d316895
Jay Bazuzi <jay@bazuzi.com>
2024-10-01 10:51:37 -0700

    @ F install dotnet to a local directory

--- _tools/run_dotnet_script.sh
+++ _tools/run_dotnet_script.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 set -euo pipefail
 
-curl --fail --silent https://dot.net/v1/dotnet-install.sh | bash -s -- --jsonfile global.json
-"$HOME/.dotnet/dotnet" tool install dotnet-script > /dev/null
-"$HOME/.dotnet/dotnet" script "$@"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+curl --fail --silent --location https://dot.net/v1/dotnet-install.sh | bash -s -- --jsonfile global.json --install-dir "${SCRIPT_DIR}/.dotnet" > /dev/null
+export PATH="${SCRIPT_DIR}/.dotnet:${PATH}"
+
+dotnet tool install dotnet-script > /dev/null
+dotnet script "$@"
