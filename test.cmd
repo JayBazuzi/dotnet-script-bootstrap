668749ab888ad2aa6d22e7786f29d80269b95f68
Jay Bazuzi <jay@bazuzi.com>
2025-01-26 22:16:42 -0800

    . t print output on failure (CMD)

--- test.cmd
+++ test.cmd
@@ -6,6 +6,6 @@ for /f "delims=" %%o in ('hello_world.cmd') do set "output=%%o"
 if "%output%"=="hello world" (
   echo Test passed
 ) else (
-  echo Test failed
+  echo Test failed; output=%output%
   exit /b 1
 )
